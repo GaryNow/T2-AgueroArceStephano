a9f4fdf11b0b2e3be3775779dc57c936bfd8778d
Stephano <nafetsgary@outlook.es>
2024-06-22 19:52:41 -0500

    creacion de componentes, servicio, ruta y guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './auth.guard';
+import { Pregunta2Component } from './dashboard/pregunta2/pregunta2.component';
+import { Pregunta3Component } from './dashboard/pregunta3/pregunta3.component';
+import { Pregunta4Component } from './dashboard/pregunta4/pregunta4.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', component: LoginComponent},
+    {path: 'dashboard', component: DashboardComponent,
+        canActivate: [authGuard],
+        children: [
+            {path: 'pregunta2', component: Pregunta2Component},
+            {path: 'pregunta3', component: Pregunta3Component},
+            {path: 'pregunta3', component: Pregunta4Component}
+        ]
+    },
+    {path: "", redirectTo: "login", pathMatch: "full"}
+   
+    
+];
